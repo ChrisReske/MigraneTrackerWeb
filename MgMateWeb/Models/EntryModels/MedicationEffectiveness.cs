b073d951907120a8212e6cc142e303c003092ab5
creske <creske@WO-NB025>
2021-09-13 11:26:27 +0200

    Modify views for 'Edit' for both 'Medication' and 'Symptoms'. Add hidden field for 'CreationDate' instead of editable text field. Create dropdown using tag helper to display enum values taken from 'MedicationEffectiveness'. Change sequence of property display in 'Create' view for 'Medication'

--- MgMateWeb/Models/EntryModels/MedicationEffectiveness.cs
+++ MgMateWeb/Models/EntryModels/MedicationEffectiveness.cs
@@ -1,10 +1,20 @@
-﻿namespace MgMateWeb.Models.EntryModels
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MgMateWeb.Models.EntryModels
 {
     public enum MedicationEffectiveness
     {
+        [Display(Name = "None")]
         None,
+        
+        [Display(Name = "Low")]
         Low,
+        
+        [Display(Name = "Medium")]
         Medium,
+        
+        [Display(Name="High")]
         High,
     }
 }
