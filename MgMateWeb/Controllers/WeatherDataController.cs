a6c31a933b265d4485d7bc441ae8deed14804c20
creske <creske@WO-NB025>
2021-09-13 16:50:55 +0200

    Create empty controller for handling weather API call. Sketch out TODO statements / comments to describe further steps.

---
+++ MgMateWeb/Controllers/WeatherDataController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MgMateWeb.Controllers
+{
+    public class WeatherDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //TODO Get Weather data from API
+        public async Task<IActionResult> GetWeatherDataAsync()
+        {
+            // Create query Url
+
+            // Create and set up WebClient
+            // Add api headers
+            var webClient = new WebClient();
+
+            // Download data
+
+            // Convert Json to object
+
+
+            // Convert object to Dto/ViewModel
+            // to be added to general entry
+
+
+
+            return null;
+        }
+
+    }
+}
