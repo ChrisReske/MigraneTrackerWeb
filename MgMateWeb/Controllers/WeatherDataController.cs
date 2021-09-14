a3333f37105352b0f11c2aae9168fa0c1fdd180d
creske <creske@WO-NB025>
2021-09-14 10:21:21 +0200

    Create new controller to retrieve weather data. Create new model to store form data required for api data retrieval. Create form for users to set weather data parameter (city, country). Create entry in 'appsettings' for api key (move to Azure Key Vault later). Create basic post method for form and data retrieval, start with getting api key first.

--- MgMateWeb/Controllers/WeatherDataController.cs
+++ MgMateWeb/Controllers/WeatherDataController.cs
@@ -1,24 +1,45 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
+using MgMateWeb.Models.WeatherModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace MgMateWeb.Controllers
 {
     public class WeatherDataController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public WeatherDataController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
 
         //TODO Get Weather data from API
-        public async Task<IActionResult> GetWeatherDataAsync()
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> GetWeatherDataAsync(WeatherDataFormModel formModel)
         {
+
+
+            // Get ApiKey
+            var apiKey = GetWeatherApiKey();
+
+            if (apiKey is null)
+                return Content("Could not retrieve ApiKey");
+            
             // Create query Url
             // city
             // country
             // units (for temperature, eg. F, or C°, set C° as default)
-            // appId (KeyVault ?)
+            // appId (later => retrieve from KeyVault)
             // sample query = https://api.openweathermap.org/data/2.5/find?q=Hannover&country=de&units=metric&appid=APIKEY
 
 
@@ -39,5 +60,13 @@ namespace MgMateWeb.Controllers
             return null;
         }
 
+        private string GetWeatherApiKey()
+        {
+            var apiKey = _configuration
+                .GetSection("ApiKeys")
+                .GetSection("OpenWeatherApiKey").Value;
+            
+            return apiKey;
+        }
     }
 }
---
+++ MgMateWeb/Models/WeatherModels/WeatherDataFormModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MgMateWeb.Models.WeatherModels
+{
+    public class WeatherDataFormModel
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        // Todo: Create Enum and Dropdown / Checkboxes for options other than 'metric'
+        public string MeasurementUnit { get; set; }
+    }
+}
