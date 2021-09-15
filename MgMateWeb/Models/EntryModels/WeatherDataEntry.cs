cfbbe8c72daa34b984656e53ee769eb7fbab8064
creske <creske@WO-NB025>
2021-09-15 09:28:51 +0200

    Add properties to model 'WeatherDataEntry'. Add new migration to apply changes to database. Update database.

--- MgMateWeb/Models/EntryModels/WeatherDataEntry.cs
+++ MgMateWeb/Models/EntryModels/WeatherDataEntry.cs
@@ -1,7 +1,15 @@
-﻿namespace MgMateWeb.Models.EntryModels
+﻿using System;
+
+namespace MgMateWeb.Models.EntryModels
 {
-    public class WeatherDataEntry : BaseEntryComponent
+    public class WeatherDataEntry
     {
-        
+        public  int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string City { get; set; }
+        public string CountryCode { get; set; }
+        public float Temperature { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
     }
 }
