44394b61e17ce69327c384baa7a75098f6a1a9fe
creske <creske@WO-NB025>
2021-09-15 16:17:19 +0200

    Change duration related properties in 'Entry' from type 'DateTime' to 'float' and adjust names to highlight that hours are being used (change this later to start and end periods). Rename property references where applicable. Add new migration and update database.

--- MgMateWeb/Models/EntryModels/Entry.cs
+++ MgMateWeb/Models/EntryModels/Entry.cs
@@ -8,14 +8,14 @@ namespace MgMateWeb.Models.EntryModels
         public int Id { get; set; }
         public DateTime CreationDate { get; set; }
         public PainIntensity PainIntensity { get; set; }
-        public DateTime PainDuration { get; set; }
+        public float HoursOfPain { get; set; }
         public List<PainType> PainTypes { get; set; }
         public List<AccompanyingSymptom> AccompanyingSymptoms { get; set; }
         public bool WasPainIncreasedDuringPhysicalActivity { get; set; }
         public List<Trigger> Triggers { get; set; }
         public List<Medication> Medications { get; set; }
-        public DateTime DurationOfIncapacitation { get; set; }
-        public DateTime DurationOfActivity { get; set; }
+        public float HoursOfIncapacitation { get; set; }
+        public float HoursOfActivity { get; set; }
         public WeatherDataEntry WeatherData { get; set; }
 
     }
