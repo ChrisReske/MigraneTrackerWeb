d04d2cff75ca1c94caf78e3c2e4b92af4097db76
creske <creske@WO-NB025>
2021-10-04 15:10:04 +0200

    Undo making properties virtual in 'EntryPainType'.

---
+++ MgMateWeb/Migrations/20211004130620_UndoMakePropertiesEntryAndPainTypeVirtual.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MgMateWeb.Migrations
+{
+    public partial class UndoMakePropertiesEntryAndPainTypeVirtual : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
