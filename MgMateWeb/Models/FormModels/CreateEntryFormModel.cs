d68d1f43a7c310c13ab9509bd0ea84ebe0ab7a1f
creske <creske@WO-NB025>
2021-10-05 09:49:56 +0200

    Create form model for creating new entries.

---
+++ MgMateWeb/Models/FormModels/CreateEntryFormModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MgMateWeb.Models.EntryModels;
+
+namespace MgMateWeb.Models.FormModels
+{
+    public class CreateEntryFormModel : Entry
+    {
+        public IEnumerable<int> SelectedAccSymptoms { get; set; }
+    }
+}
