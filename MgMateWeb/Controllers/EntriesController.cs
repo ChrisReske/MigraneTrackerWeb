c83468f88e1ec58de3c898f899050c7b891bc600
creske <creske@WO-NB025>
2021-10-05 09:43:54 +0200

    Scaffold 'Entries' controller. Modify scaffolded code according to personal / corporate code style. Add navigation link to 'Entries/Index' to main navigation bar.

---
+++ MgMateWeb/Controllers/EntriesController.cs
@@ -0,0 +1,170 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MgMateWeb.Models.EntryModels;
+using MgMateWeb.Persistence;
+
+namespace MgMateWeb.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EntriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Entries
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Entries
+                .ToListAsync()
+                .ConfigureAwait(false));
+        }
+
+        // GET: Entries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entries
+                .FirstOrDefaultAsync(m => m.Id == id)
+                .ConfigureAwait(false);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return View(entry);
+        }
+
+        // GET: Entries/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Entries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CreationDate,PainDuration,WasPainIncreasedDuringPhysicalActivity,DurationOfIncapacitation,DurationOfActivity")] Entry entry)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(entry);
+            }
+
+            _context.Add(entry);
+            await _context
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Entries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entries
+                .FindAsync(id)
+                .ConfigureAwait(false);
+            
+            if (entry == null)
+            {
+                return NotFound();
+            }
+            return View(entry);
+        }
+
+        // POST: Entries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CreationDate,PainDuration,WasPainIncreasedDuringPhysicalActivity,DurationOfIncapacitation,DurationOfActivity")] Entry entry)
+        {
+            if (id != entry.Id)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(entry);
+            }
+
+            try
+            {
+                _context.Update(entry);
+                await _context
+                    .SaveChangesAsync()
+                    .ConfigureAwait(false);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EntryExists(entry.Id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Entries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entries
+                .FirstOrDefaultAsync(m => m.Id == id)
+                .ConfigureAwait(false);
+
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return View(entry);
+        }
+
+        // POST: Entries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var entry = await _context.Entries
+                .FindAsync(id)
+                .ConfigureAwait(false);
+           
+            _context.Entries.Remove(entry);
+            
+            await _context
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EntryExists(int id)
+        {
+            return _context.Entries.Any(e => e.Id == id);
+        }
+    }
+}
